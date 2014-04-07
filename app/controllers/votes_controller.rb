521b0a5103d1d68f28ffa3396e66cb28b8b3f316
szymzet <sz.zabkiewicz@gmail.com>
2014-04-07 10:48:10 +0200

    Votes controller refactoring - extract method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,8 @@ class VotesController < ApplicationController
 
   def update
     poll = Poll.find(params[:poll_id])
-    if !poll.require_login || user_signed_in?
-      vote_for_question(poll, params[:id])
-    else
-      flash[:alert] = 'Logging in is required before taking part in this poll'
-    end
-    return redirect_to hashed_poll_path(poll.hashed_id)
+    try_voting_for(poll)
+    redirect_to hashed_poll_path(poll.hashed_id)
   end
 
   private
@@ -19,5 +15,13 @@ class VotesController < ApplicationController
     question.vote!
     question.save
     mark_as_voted_for(poll.id)
+  end
+
+  def try_voting_for(poll)
+    if !poll.require_login || user_signed_in?
+      vote_for_question(poll, params[:id])
+    else
+      flash[:alert] = 'Logging in is required before taking part in this poll'
+    end
   end
 end
