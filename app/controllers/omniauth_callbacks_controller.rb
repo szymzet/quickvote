d3c871d2d2e137d0e3c494eb2dad3c513f8658aa
szymzet <sz.zabkiewicz@gmail.com>
2014-04-10 12:06:30 +0200

    Redirect to last page after signing in

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class OmniauthCallbacksController < ApplicationController
   def google_oauth2
     @user = User.find_for_google_oauth2(request.env['omniauth.auth'])
-    sign_in_and_redirect @user, event: :authentication
+    sign_in @user, event: :authentication
+    redirect_to(request.env['omniauth.origin'] || root_path)
   end
 end
